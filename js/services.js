6da9cd6b4b56e7fff262b6b37428b25ae2bc07d1
nathanmccartney <nmccartney1231@gmail.com>
2013-10-29 17:41:02 -0400

    changed dependency on server json

--- js/services.js
+++ js/services.js
@@ -1,9 +1,9 @@
 angular.module('airlineServices', ['ngResource'])
 	.factory('Airport', function  ($resource) {
-		return $resource('/airports/:airportCode');
+		return $resource('/airports.json');
 	})
 	.factory('Flights', function  ($resource) {
-		return $resource('/flights');
+		return $resource('/flights.json');
 	})
 	.factory('Reservations', function  ($resource) {
 		return $resource('/reservations');
